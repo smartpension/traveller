2d9d1d55b5b83fb8d570b4428d174dc527180c48
rorilon <kseniia.stanishevskaia@smartpension.com>
2021-11-30 14:21:58 +0000

    (feat) add fonts

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -8,15 +8,23 @@ import {
   InputRightElement,
   Input,
   Heading,
-  theme,
   InputGroup,
   IconButton,
+  extendTheme,
 } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
 import { TopBar } from './TopBar'
 
+const fonts = {
+  heading:
+    '"Museo Sans", museo-sans, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  body: '"Lato", lato, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+  // chakra default
+  mono: 'SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+}
+
 export const App: FC = () => (
-  <ChakraProvider theme={theme}>
+  <ChakraProvider theme={extendTheme({ fonts })}>
     <Box textAlign="center">
       <Grid minH="100vh" p={3} gridTemplateRows="auto auto 1fr">
         <TopBar />
