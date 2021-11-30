11cae4a7addc409931b713fae62d730b91e1b4b6
Gosia Heba <malgorzata.heba@smartpension.co.uk>
2021-11-30 14:28:09 +0100

    Add router for the /rest/cities endpoint

---
+++ packages/api/src/cities/routes.ts
@@ -0,0 +1,26 @@
+import type { Request } from 'express'
+import express from 'express'
+import type { City } from 'src/data/cities'
+import { citiesService } from './service'
+
+export const citiesRouter = express.Router()
+
+citiesRouter.get('/', (req: Request<unknown, unknown, unknown, Partial<City>>, res) => {
+  res.send(citiesService.getAll(req.query))
+})
+
+citiesRouter.get('/:cityId', (req, res) => {
+  const city = citiesService.get(req.params.cityId)
+  if (city) return res.send(city)
+
+  // @TODO: add better error handling instead of inline objects
+  res.status(404).send({ status: 404, message: 'Not found' })
+})
+
+citiesRouter.put('/:cityId', (req, res) => {
+  const updatedCity = citiesService.update(req.params.cityId, req.body)
+  if (updatedCity) return res.send(updatedCity)
+
+  // @TODO: add better error handling instead of inline objects
+  res.status(404).send({ status: 404, message: 'Not found' })
+})
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,12 +1,10 @@
 import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
-import type { Request } from 'express'
 import express from 'express'
 import http from 'http'
 import { typeDefs } from './graphql/typeDefs'
 import { resolvers } from './graphql/resolvers'
-import { citiesService } from './cities/service'
-import type { City } from './data/cities'
+import { citiesRouter } from './cities/routes'
 
 async function startApolloServer(typeDefs, resolvers) {
   const app = express()
@@ -25,26 +23,7 @@ async function startApolloServer(typeDefs, resolvers) {
   })
 
   app.use(express.json())
-
-  app.get('/rest/cities', (req: Request<unknown, unknown, unknown, Partial<City>>, res) => {
-    res.send(citiesService.getAll(req.query))
-  })
-
-  app.get('/rest/cities/:cityId', (req, res) => {
-    const city = citiesService.get(req.params.cityId)
-    if (city) return res.send(city)
-
-    // @TODO: add better error handling instead of inline objects
-    res.status(404).send({ status: 404, message: 'Not found' })
-  })
-
-  app.put('/rest/cities/:cityId', (req, res) => {
-    const updatedCity = citiesService.update(req.params.cityId, req.body)
-    if (updatedCity) return res.send(updatedCity)
-
-    // @TODO: add better error handling instead of inline objects
-    res.status(404).send({ status: 404, message: 'Not found' })
-  })
+  app.use('/rest/cities', citiesRouter)
 
   await new Promise<void>(resolve => httpServer.listen({ port: 4000 }, resolve))
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
