ad9bb7925281e54e29eea96eab6bf903a4c3b43f
Oli Wright <oli.wright@blueyonder.co.uk>
2021-11-30 16:54:54 +0000

    Add 404 response

--- packages/api/src/graphql/resolvers.ts
+++ packages/api/src/graphql/resolvers.ts
@@ -1,6 +1,7 @@
 import type { City } from '../cities/types'
 import { citiesService } from '../cities/service'
 import { isDefined } from '../utils'
+import { HttpQueryError } from 'apollo-server-core'
 
 type UpdateCityArgs = Partial<Pick<City, 'visited' | 'wishlist'>> & Pick<City, 'id'>
 
@@ -10,8 +11,13 @@ export const resolvers = {
     cities: (_: undefined, { id, country, name, visited, wishlist }: City): City[] => {
       return citiesService.getAll({ id, country, name, visited, wishlist })
     },
+
     city: (_: undefined, { id }: City): City | undefined => {
-      return citiesService.get(id)
+      const city = citiesService.get(id)
+
+      if (city) return city
+
+      throw new HttpQueryError(404, `A city with id:${id} could not be found`)
     },
   },
 
