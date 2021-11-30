f20f08d5e4316752fef00cf1e64d255ddb8b1809
franciskone <francesco.cappelli.developer@gmail.com>
2021-11-30 11:32:25 +0000

    refine unit test example

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -3,8 +3,10 @@ import { screen } from '@testing-library/react'
 import { render } from './test-utils'
 import { App } from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn chakra/i)
-  expect(linkElement).toBeInTheDocument()
+describe('<App /> component', () => {
+  it('renders the Header content', () => {
+    render(<App />)
+    const HeadingComponent = screen.getByText(/^Smart traveler$/i)
+    expect(HeadingComponent).toBeInTheDocument()
+  })
 })
