679bb4f9ce5f36b757bfade3f94055e41c136335
Oli Wright <oli.wright@blueyonder.co.uk>
2021-11-30 17:18:13 +0000

    [] - Rfactor cities resolver args

--- packages/api/src/graphql/resolvers.ts
+++ packages/api/src/graphql/resolvers.ts
@@ -1,23 +1,25 @@
-import type { City } from '../cities/types'
+import type { City, CitiesFilter } from '../cities/types'
 import { citiesService } from '../cities/service'
 import { isDefined } from '../utils'
 import { HttpQueryError } from 'apollo-server-core'
 
 type UpdateCityArgs = Partial<Pick<City, 'visited' | 'wishlist'>> & Pick<City, 'id'>
+type CityArgs = Pick<City, 'id'>
 
 export const resolvers = {
   Query: {
-    // @TODO: add single city resolver
-    cities: (_: undefined, { id, country, name, visited, wishlist }: City): City[] => {
+    cities: (_: undefined, args: CitiesFilter): City[] => {
+      const { id, country, name, visited, wishlist } = args.filter
+
       return citiesService.getAll({ id, country, name, visited, wishlist })
     },
 
-    city: (_: undefined, { id }: City): City | undefined => {
+    city: (_: undefined, { id }: CityArgs): City => {
       const city = citiesService.get(id)
 
-      if (city) return city
+      if (!city) throw new HttpQueryError(404, `A city with id: ${id} could not be found`)
 
-      throw new HttpQueryError(404, `A city with id:${id} could not be found`)
+      return city
     },
   },
 
@@ -27,7 +29,7 @@ export const resolvers = {
       isDefined(wishlist) && Object.assign(fieldsToUpdate, { wishlist })
       const updatedCity = citiesService.update(id, fieldsToUpdate)
 
-      if (!updatedCity) throw new Error(`Cannot find a city of id: ${id}`)
+      if (!updatedCity) throw new HttpQueryError(404, `A city with id: ${id} could not be found`)
 
       return updatedCity
     },
