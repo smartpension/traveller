92d1c0c543bab7ca28d47cd9ce094645f8298257
Gosia Heba <malgorzata.heba@smartpension.co.uk>
2021-12-01 10:17:22 +0100

    Use destructuring in the errorHandler

--- packages/api/src/middleware/errorHandler.ts
+++ packages/api/src/middleware/errorHandler.ts
@@ -1,8 +1,12 @@
 import type { Request, Response, NextFunction } from 'express'
 import type { HttpError } from 'src/common'
 
-export const errorHandler = (httpError: HttpError, _req: Request, res: Response, _next: NextFunction): void => {
-  const status = httpError.statusCode || 500
-  const message = httpError.message
+export const errorHandler = (
+  { statusCode, message }: HttpError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+): void => {
+  const status = statusCode || 500
   res.status(status).send({ status, message })
 }
