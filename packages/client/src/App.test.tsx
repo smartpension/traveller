10827d9ae8133cd516ce2e9b5793dda3ad920df3
Sam De Maeyer <sam_demaeyer@hotmail.com>
2022-01-07 18:19:06 +0100

    Add BrowserRouter to App.test.tsx

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { screen } from '@testing-library/react'
 import { render } from './test-utils'
 import { App } from './App'
 
 describe('<App /> component', () => {
   it('renders the Header content', () => {
-    render(<App />)
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
     const HeadingComponent = screen.getByText(/^Smart traveller$/i)
     expect(HeadingComponent).toBeInTheDocument()
   })
